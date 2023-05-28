8c8d0b74dc5a7a38ad422d6e8a282c831f0d62f2
JaQeQe Jackson <115498054+MyzticTN@users.noreply.github.com>
2023-05-28 08:24:02 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -18,7 +18,7 @@ server.on('upgrade', (req, socket, head) => {
   socket.end();
 });
 
-const PORT = process.env.PORT || 80; // Specify the port number you want to use
+const PORT = process.env.PORT || 443; // Specify the port number you want to use
 
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
